c328c932d2dfefd7a2f27367cb3ad4bf62615ac0
kohlbrr <kohlbrr@gmail.com>
2017-08-28 17:12:08 -0400

    Bugfix(osx_audit): Software restriction check was not set correctly prior; added comments

--- scripts/osx_audit.py
+++ scripts/osx_audit.py
@@ -1,25 +1,37 @@
+'''
+This is a script to check the current fitness of a given Mac.
+
+Run with `sudo python osx_audit.py`
+Tested on OSX 10.10
+'''
+
 import subprocess
 from distutils.version import StrictVersion
 import re
 
+# Subprocess function to execute shell commands
 def sp_cmd(command):
   process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
   proc_stdout = process.communicate()[0].strip()
   return proc_stdout
 
+# Helper function to convert bytes to GBs
 def get_gb(bytes):
   return ((bytes / 1024) / 1024) / 1024
 
+# Helper function to return a status string based off a boolean
 def status(boolean):
   return 'Valid -' if boolean else 'INVALID -'
 
 # OSX Version
+# Checks if the OSX version is greater or equal to 10.11
 osv = sp_cmd('sw_vers -productVersion')
 print 'OSX Version:', \
-  status(StrictVersion(osv) >= StrictVersion('10.7.0')), \
+  status(StrictVersion(osv) >= StrictVersion('10.11.0')), \
   osv
 
 # RAM
+# Checks if the system has at least 8GB of RAM
 ram = sp_cmd('sysctl hw.memsize')
 ramGb = get_gb(int(re.findall(r'\d+', ram)[0]))
 print 'RAM Size:', \
@@ -28,6 +40,7 @@ print 'RAM Size:', \
   'GB'
 
 # Disk Usage
+# Ensures the main partition is not 75% or more full
 disk = sp_cmd('df -k | grep /dev/')
 diskUsage = int(re.findall(r'\d+', disk)[7])
 print 'Disk Usage:', \
@@ -36,6 +49,7 @@ print 'Disk Usage:', \
   '%'
 
 # /tmp Size
+# Checks to see how large /tmp is
 tmp = sp_cmd('du -k /tmp')
 tmpSize = int(re.findall(r'\d+', tmp)[0])
 print '/tmp Size:', \
@@ -44,20 +58,24 @@ print '/tmp Size:', \
   'KB'
 
 # Filevault
+# Determines if Filevault is enabled
 filevault = sp_cmd('fdesetup status')
 print 'Filevault:', \
   status("On" in filevault), \
   filevault
 
 # SSH Access
+# REQUIRES SUDO
+# Checks to see if SSH access is enabled in sharing settings
 ssh = sp_cmd('systemsetup -getremotelogin')
 print 'SSH Access:', \
   status("Off" in ssh), \
   ssh
 
 # Software Install Restrictions
-restrictions = sp_cmd('systemsetup -getremotelogin')
+# Sees if there are any install restrictions set under Security and Privacy
+restrictions = sp_cmd('spctl --status')
 print 'Software Install Restrictions', \
-  status("On" in restrictions), \
+  status("enabled" in restrictions), \
   restrictions
 
